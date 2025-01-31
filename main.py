58e0c73a1ea35c6add027bec88ed6218b7e4b036
Millan Kumar <millankumar@gmail.com>
2025-01-31 07:39:35 -0500

    Slightly cleaning code

--- main.py
+++ main.py
@@ -47,6 +47,15 @@ CAP_ARGS = "v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_2D4AA0A0-video
 #         if cv2.waitKey(1) & 0xFF == ord('q'):
 #             break
 
+
+class Dectection:
+    def __init__(self, id, cnt, approx, cx, cy):
+        self.id = id
+        self.cnt = cnt
+        self.approx = approx
+        self.cx = cx
+        self.cy = cy
+
 def image_read(fname):
     frame = cv2.imread(fname)
 
@@ -54,10 +63,10 @@ def image_read(fname):
 
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # detect green color
+    # detect grass: yellow/green color
     frame_threshold = cv2.inRange(frame_hsv, (25, 25, 25), (80, 255,255))
 
-    # detect grey and black
+    # detect: track: grey and black
     # frame_threshold_1 = cv2.inRange(frame_hsv, (0, 10, 25), (25, 255, 150))
     # frame_threshold_2 = cv2.inRange(frame_hsv, (80, 0, 0), (180, 255, 150))
     # frame_threshold = cv2.bitwise_or(frame_threshold_1, frame_threshold_2)
@@ -71,9 +80,10 @@ def image_read(fname):
     contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
 
-    all_contours = [] # [(cnt, approx, cx, cy), ...]
+    all_dections = []
 
-    for cnt in contours:
+    # for cnt in contours:
+    for i, cnt in enumerate(contours):
         # x, y, w, h = cv2.boundingRect(cnt)
         approx = cv2.approxPolyDP(cnt, 0.03*cv2.arcLength(cnt, True), True)
         cv2.drawContours(frame, [approx], 0, (0, 255, 255), 2)
@@ -84,36 +94,38 @@ def image_read(fname):
 
         cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
 
-        all_contours.append((cnt, approx, cx, cy))
+        detection = Dectection(i, cnt, approx, cx, cy)
+        all_dections.append(detection)
 
-    left_most = min(all_contours, key=lambda x: x[2])
-    right_most = max(all_contours, key=lambda x: x[2])
+
+    left_most = min(all_dections, key=lambda x: x.cx)
+    right_most = max(all_dections, key=lambda x: x.cx)
 
     rightest_idx = 0
-    rightest_idx_2 = 1
     rightest_value = 0
 
-    approx = left_most[1]
-    for i in range(len(approx)):
-        pt1 = approx[i]
-        i2 = (i + 1) % len(approx)
-        pt2 = approx[i2]
+    for i in range(len(left_most.approx)):
+        pt1 = left_most.approx[i]
+        i2 = (i + 1) % len(left_most.approx)
+        pt2 = left_most.approx[i2]
 
         x1, y1 = pt1[0]
         x2, y2 = pt2[0]
+
+        cv2.circle(frame, (x1, y1), 5, (255, 255, 0), -1)
 
         d = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
         if d > rightest_value:
             rightest_value = d
             rightest_idx = i
-            rightest_idx_2 = i2
 
-    cv2.line(frame, approx[rightest_idx][0], approx[rightest_idx_2][0], (255, 0, 255), 5)
+    rightest_idx_2 = (rightest_idx + 1) % len(left_most.approx)
+    cv2.line(frame, left_most.approx[rightest_idx][0], left_most.approx[rightest_idx_2][0], (255, 0, 255), 5)
 
 
-    cv2.drawContours(frame, [left_most[1]], 0, (255, 0, 0), 2)
-    cv2.drawContours(frame, [right_most[1]], 0, (255, 0, 0), 2)
+    cv2.drawContours(frame, [left_most.approx], 0, (255, 0, 0), 2)
+    cv2.drawContours(frame, [right_most.approx], 0, (255, 0, 0), 2)
 
 
     # grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)

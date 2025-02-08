24850e729966a99c01755d82a990370443da4410
Millan Kumar <millankumar@gmail.com>
2025-02-07 20:58:53 -0500

    Trying to clean the code (section dividers)

--- hold.py
+++ hold.py
@@ -3,6 +3,8 @@ This file contains code that we might want to use in the future but it otherwise
 """
 
 
+# ap.add_argument("-z", "--video-capture-zero", required=False, help="testing version", action="store_true")
+
 # CAMERA_WAIT_AFTER_ACTIVE = 1.0
 # CAP_ARGS = "v4l2src device=/dev/v4l/by-id/usb-046d_C270_HD_WEBCAM_2D4AA0A0-video-index0 ! video/x-raw,width=320,height=240,framerate=30/1 ! videoconvert ! appsink"
 

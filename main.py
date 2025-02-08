521f209ff0aa92194ea49fe5336c1d0e156fdda7
Millan Kumar <millankumar@gmail.com>
2025-02-07 21:33:37 -0500

    ADDING COMMENTS (and cleaning the code a bit)

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import util
 
 
 # ---------------------------------------------------------------------------- #
-BOTTOM_RATIO = 9/10
+BOTTOM_RATIO = 19/20
 TARGET_Y_RATIO = 6/10
 # ---------------------------------------------------------------------------- #
 
@@ -20,18 +20,17 @@ TARGET_Y_RATIO = 6/10
 class Detection:
     """Structure to hold detection information"""
 
-    def __init__(self, id, cnt, approx, cx, cy, line_start_idx, edge_slope, camera_slope, lower_pt, higher_pt):
-        self.id = id
-        self.cnt = cnt
-        self.approx = approx
-        self.cx = cx
-        self.cy = cy
-        self.line_start_idx = line_start_idx
-        self.line_end_idx = (line_start_idx + 1) % len(approx)
-        self.edge_slope = edge_slope
-        self.camera_slope = camera_slope
-        self.lower_pt = lower_pt
-        self.higher_pt = higher_pt
+    def __init__(self, cnt, approx, cx, cy, edge_slope, camera_slope, lower_pt, higher_pt):
+        self.cnt = cnt # perfect/exact contour
+        self.approx = approx # approximated polygon
+        self.cx = cx # center x of contour
+        self.cy = cy # center y of contour
+        # self.line_start_idx = line_start_idx # index in self.approx of the start of the track side edge
+        # self.line_end_idx = (line_start_idx + 1) % len(approx) # index in self.approx of the end of the track side edge
+        self.edge_slope = edge_slope # slope of the track side edge
+        self.camera_slope = camera_slope # slope between the bottom point and the bottom center
+        self.lower_pt = lower_pt # the lower point of the track side edge
+        self.higher_pt = higher_pt # the higher point of the track side edge
 # ---------------------------------------------------------------------------- #
 
 
@@ -73,12 +72,16 @@ def find_contours(frame, old_dilated):
 
 
 def handle_video(fname):
+    """Open video, read frames, process each frame, and display"""
+
     cap = cv2.VideoCapture(fname)
 
+    # Will combine last dilated mask with the current to improve detection
     last_dilated = None
 
     while True:
         ret, frame = cap.read()
+
         if not ret:
             print("Error reading frame...")
             break
@@ -86,9 +89,7 @@ def handle_video(fname):
         dilated = image_read(frame, last_dilated)
         last_dilated = dilated
 
-        # if cv2.waitKey(33) & 0xFF == ord('q'):
-        #     break
-        # w to pause
+        # w to toggle pause, q to quit
         key = cv2.waitKey(33) & 0xFF
         if key == ord('w'):
             cv2.waitKey(-1)
@@ -96,52 +97,61 @@ def handle_video(fname):
             break
 
 
-def image_read(frame, old_dilated):
 # def image_read(fname):
-    # frame = cv2.imread(fname)
+# frame = cv2.imread(fname)
 
+def image_read(frame, old_dilated):
+    # Calculate the bottom center of the frame: used for drawing intersection lines with polygon edges to find the track edges
     bottom_center = (frame.shape[1] // 2, int(frame.shape[0] * BOTTOM_RATIO))
+    # Calculate the target y which is a constant distance from the kart
     target_y = int(frame.shape[0] * TARGET_Y_RATIO)
 
+    # Find contours
     contours, dilated, total_diated = find_contours(frame, old_dilated)
-    frame = cv2.bitwise_and(frame, frame, mask=total_diated)
 
+    # Debug drawining
+    frame = cv2.bitwise_and(frame, frame, mask=total_diated) # only show the detected area
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 1) # draw perfect outlines of grass
 
-    cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
-
-    cv2.line(frame, (frame.shape[1] // 2, frame.shape[0]), (frame.shape[1] // 2, 0), (255, 255, 0), 1)
-    cv2.line(frame, (0, target_y), (frame.shape[1], target_y), (255, 255, 0), 2)
+    cv2.line(frame, (frame.shape[1] // 2, frame.shape[0]), (frame.shape[1] // 2, 0), (255, 255, 0), 1) # vertical center line
+    cv2.line(frame, (0, target_y), (frame.shape[1], target_y), (255, 255, 0), 2) # horizontal target line
 
 
     all_detections = []
 
-    # for cnt in contours:
-    for i, cnt in enumerate(contours):
-        # x, y, w, h = cv2.boundingRect(cnt)
+    for cnt in contours:
+        # Approximate the contour to a polygon (find straight edges)
         approx = cv2.approxPolyDP(cnt, 0.03*cv2.arcLength(cnt, True), True)
-        cv2.drawContours(frame, [approx], 0, (0, 255, 255), 2)
+        cv2.drawContours(frame, [approx], 0, (0, 255, 255), 2) #  draw the approximated polygon
 
+        # Calculate the center of the contour
         center = cv2.moments(cnt)
         if center["m00"] == 0:
             center["m00"] = 1
         cx = int(center["m10"] / center["m00"])
         cy = int(center["m01"] / center["m00"])
 
+        # Throw out all contours that are in the center
+        # In the center means it is probably not a track edge but trees or other objects
         left_33 = int(frame.shape[1] * (1/3))
         right_33 = int(frame.shape[1] * (2/3))
         if left_33 < cx < right_33:
             continue
 
-
+        # Debug drawing: center of contour
         cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
 
+
+        # Find the edge of the polygon that that is the track edge
+        # Works by drawing a line from the bottom center to the center of the contour
+        # And the closest intersection with the polygon is the edge
         intersection_idx = len(approx) - 1
         intersection_dist = float("inf")
         edge_slope = 0
 
         for j in range(len(approx)):
             pt1 = approx[j][0]
-            i2 = (j + 1) % len(approx)
+            i2 = (j + 1) % len(approx) # handle wrap around
             pt2 = approx[i2][0]
 
             intersection = util.line_intersection(pt1, pt2, bottom_center, (cx, cy))
@@ -158,78 +168,96 @@ def image_read(frame, old_dilated):
                     else:
                         edge_slope = dy / dx
 
-        
+        # Debug drawing: the track side edge
         i2 = (intersection_idx + 1) % len(approx)
         cv2.line(frame, approx[intersection_idx][0], approx[i2][0], (0, 0, 255), 3)
 
+        # Calculate the lower and higher point of the edge
+        # Needed for later, but also using it to pick which contour to track if multiple contours are detected
         pt1 = approx[intersection_idx][0]
         pt2 = approx[i2][0]
         lower_pt = pt1 if pt1[1] > pt2[1] else pt2
         higher_pt = pt1 if pt1[1] < pt2[1] else pt2
 
-        dx = lower_pt[0] - bottom_center[0]
-        dy = lower_pt[1] - bottom_center[1]
+        # dx = cx - bottom_center[0]
+        # dy = cy - bottom_center[1]
+
+        # dx = lower_pt[0] - bottom_center[0]
+        # dy = lower_pt[1] - bottom_center[1]
         if dx == 0:
             camera_slope = 999999
         else:
             camera_slope = dy / dx
 
+        # Trying to throw out contours that are probably noise
         # if (camera_slope < 0 and edge_slope < 0) or (camera_slope > 0 and edge_slope > 0):
         #     continue
         # if (camera_slope > 0 and cx > frame.shape[1] // 2) or (camera_slope < 0 and cx < frame.shape[1] // 2):
         #     continue
 
-        detection = Detection(i, cnt, approx, cx, cy, intersection_idx, edge_slope, camera_slope, lower_pt, higher_pt)
+        detection = Detection(cnt, approx, cx, cy, edge_slope, camera_slope, lower_pt, higher_pt)
         all_detections.append(detection)
 
-
+    # Sort by camera slope (slope between the bottom center and the bottom point of the track edge)
+    # Lower number means lower on the screen
     track_detections = sorted(all_detections, key=lambda x: abs(x.camera_slope))
 
+    # Right now only handling the cases were we know both edges
     if len(track_detections) >= 2:
-        # track_detections = track_detections[:min(2, len(track_detections))]
         edges = [track_detections[0]]
+
+        # Don't pick two edges that are on the same side of the screen
         if edges[0].cx >= frame.shape[1] // 2:
             track_detections = list(filter(lambda x: x.cx < frame.shape[1] // 2, track_detections[1:]))
         else:
             track_detections = list(filter(lambda x: x.cx > frame.shape[1] // 2, track_detections[1:]))
         
-        if len(track_detections) > 0:
-            edges.append(track_detections[0])
+        if len(track_detections) > 0: # At least one other detection on the other side
+            edges.append(track_detections[0]) # Used [1:] earlier so don't have to worry about repeating
 
+            # Debug drawing: line between the bottom center and the bottom point of the track edge
             for detection in edges:
-                # cv2.line(frame, (detection.lower_pt[0], detection.lower_pt[1]), bottom_center, (255, 0, 0), 2)
-                cv2.line(frame, (detection.cx, detection.cy), bottom_center, (255, 0, 0), 1)
+                cv2.line(frame, (detection.lower_pt[0], detection.lower_pt[1]), bottom_center, (255, 0, 0), 2)
+                # cv2.line(frame, (detection.cx, detection.cy), bottom_center, (255, 0, 0), 1)
 
-            # edges = sorted(edges, key=lambda x: abs(x.edge_slope), reverse=True)
-            # closest_detection = edges[0]
-            # cv2.line(frame, (closest_detection.lower_pt[0], closest_detection.lower_pt[1]), bottom_center, (255, 100, 0), 3)
-
+            # Find the lowest and highest points of the two edges
             lowest_point = edges[0].lower_pt if edges[0].lower_pt[1] > edges[1].lower_pt[1] else edges[1].lower_pt
             highest_point = edges[0].higher_pt if edges[0].higher_pt[1] < edges[1].higher_pt[1] else edges[1].higher_pt
 
+            # Extend the edges so they start and end at the same heights
             edge_0_ext = util.extend_segment(edges[0].lower_pt, edges[0].higher_pt, lowest_point[1], highest_point[1])
             edge_1_ext = util.extend_segment(edges[1].lower_pt, edges[1].higher_pt, lowest_point[1], highest_point[1])
 
+            # Debug drawing: extended edges
             cv2.line(frame, (int(edge_0_ext[0][0]), int(edge_0_ext[0][1])), (int(edge_0_ext[1][0]), int(edge_0_ext[1][1])), (0, 128, 255), 2)
             cv2.line(frame, (int(edge_1_ext[0][0]), int(edge_1_ext[0][1])), (int(edge_1_ext[1][0]), int(edge_1_ext[1][1])), (0, 128, 255), 2)
 
-
+            # Find the midpoint of the two extended edges which should be the middle of the track
             lower_midpoint = ((int(edge_0_ext[0][0]) + int(edge_1_ext[0][0])) // 2, (int(edge_0_ext[0][1]) + int(edge_1_ext[0][1])) // 2)
             higher_midpoint = ((int(edge_0_ext[1][0]) + int(edge_1_ext[1][0])) // 2, (int(edge_0_ext[1][1]) + int(edge_1_ext[1][1])) // 2)
-            cv2.line(frame, lower_midpoint, higher_midpoint, (255, 0, 255), 4)
+            cv2.line(frame, lower_midpoint, higher_midpoint, (255, 0, 255), 4) # draw the middle of the track
 
-
+            # Find the higher low and the lower high point of the two edges
             bot_point = edges[0].lower_pt if edges[0].lower_pt[1] < edges[1].lower_pt[1] else edges[1].lower_pt
             top_point = edges[0].higher_pt if edges[0].higher_pt[1] > edges[1].higher_pt[1] else edges[1].higher_pt
-
+            
+            # Shorten the edges so they start and end at the same heights
             edge_0_ext = util.extend_segment(edges[0].lower_pt, edges[0].higher_pt, bot_point[1], top_point[1])
             edge_1_ext = util.extend_segment(edges[1].lower_pt, edges[1].higher_pt, bot_point[1], top_point[1])
 
+            # Debug drawing: shortened edges
+            cv2.line(frame, (int(edge_0_ext[0][0]), int(edge_0_ext[0][1])), (int(edge_0_ext[1][0]), int(edge_0_ext[1][1])), (128, 255, 128), 2)
+            cv2.line(frame, (int(edge_1_ext[0][0]), int(edge_1_ext[0][1])), (int(edge_1_ext[1][0]), int(edge_1_ext[1][1])), (128, 255, 128), 2)
+
+            # Find the midpoint of the two shortened edges which should be the middle of the track
+            # This is on the same line as the longer line, but it is a good debug reference
             lower_midpoint = ((int(edge_0_ext[0][0]) + int(edge_1_ext[0][0])) // 2, (int(edge_0_ext[0][1]) + int(edge_1_ext[0][1])) // 2)
             higher_midpoint = ((int(edge_0_ext[1][0]) + int(edge_1_ext[1][0])) // 2, (int(edge_0_ext[1][1]) + int(edge_1_ext[1][1])) // 2)
             cv2.line(frame, lower_midpoint, higher_midpoint, (255, 0, 128), 4)
 
-
+            # Calculate (in camera space) where we want to go
+            # This is the intersection of the middle of the track and the target line (which is a constant y value or
+            #   a constant distance from the kart)
             fully_extended = util.extend_segment(lower_midpoint, higher_midpoint, 0, frame.shape[0])
             intersection = util.line_intersection(fully_extended[0], fully_extended[1], (0, target_y), (frame.shape[1], target_y))
             if intersection is not None:
@@ -240,8 +268,10 @@ def image_read(frame, old_dilated):
     else:
         print(f"DETECTION LOST: {len(track_detections)}")
 
-
+    # Display the resulting frame
     cv2.imshow("frame", frame)
+
+    # Return the dilated mask for the next frame
     return dilated
 
     # if cv2.waitKey(0) & 0xFF == ord('q'):

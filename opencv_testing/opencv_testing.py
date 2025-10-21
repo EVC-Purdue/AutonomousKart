af77f62ac2c1a3968cb177b1a5688474369e68ff
Hirthik Gopal Shanmugam <141051449+hgs2007@users.noreply.github.com>
2025-10-21 18:24:01 -0400

    calculates speed, steering input, and acceleration using angles

--- opencv_testing/opencv_testing.py
+++ opencv_testing/opencv_testing.py
@@ -1,15 +1,15 @@
 import cv2
 
-cap = cv2.VideoCapture("IMG_8824.mp4")  # or 0 for webcam
+cap = cv2.VideoCapture("IMG_8824.mp4") 
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        break  # end of video
+        break  
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    cv2.imshow("Video", gray_frame)  # show the video in a window
+    cv2.imshow("Video", gray_frame)  #shows the video
 
     # Press 'q' to exit early
     if cv2.waitKey(25) & 0xFF == ord('q'):

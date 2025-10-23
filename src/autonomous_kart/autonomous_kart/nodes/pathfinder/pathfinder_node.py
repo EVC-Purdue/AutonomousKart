4c4c5f734d382866a596580cc5c953574bc465e8
shay <shay.manor@gmail.com>
2025-10-23 18:38:56 -0400

    chore(pathfinder): added params and set publisher to publish these params for 5 seconds then stop

--- src/autonomous_kart/autonomous_kart/nodes/pathfinder/pathfinder_node.py
+++ src/autonomous_kart/autonomous_kart/nodes/pathfinder/pathfinder_node.py
@@ -17,6 +17,18 @@ class PathfinderNode(Node):
         self.declare_parameter('system_frequency', 60)
         self.system_frequency = self.get_parameter('system_frequency').value
 
+        self.declare_parameter('parking_lot', False)
+        self.parking_lot = self.get_parameter('parking_lot').value
+
+        if self.parking_lot:
+            self.declare_parameter('speed', 5)
+            self.speed = self.get_parameter('speed').value
+
+            self.declare_parameter('steering', 0)
+            self.steering = self.get_parameter('steering').value
+
+            self.running = True
+
         # Timer to log average every 5 seconds
         self.create_timer(5.0, self.log_command_rate)
 
@@ -47,14 +59,22 @@ class PathfinderNode(Node):
     def calculate_path_callback(self, msg: Float32MultiArray):
         """
         Calculate commands for steering and motor from opencv_pathfinder efficiently
-        Part of hot loop so must be efficient
+        Part of hot loop so must be efficient.
+        If parking lot mode, drive for 5 seconds at speed with steering then stop.
         :param msg: Float32MultiArray of [left angle from center to base of track from image, right angle ...]
         :return: Publishes commands to motor & steering
         """
         self.cmd_count += 1
         self.angles = (msg.data[0], msg.data[1])
+        if not self.parking_lot:
+            if not self.running:
+                return
+            if self.cmd_count > 5 * self.system_frequency:  # 5 seconds
+                self.running = False
 
-        motor_speed, steering_angle = pathfinder(msg.data)
+            motor_speed, steering_angle = pathfinder(msg.data)
+        else:
+            motor_speed, steering_angle = self.speed, self.steering
 
         self.steering_publisher.publish(Float32(data=steering_angle))
         self.motor_publisher.publish(Float32(data=motor_speed))
@@ -74,6 +94,7 @@ class PathfinderNode(Node):
         # Reset counters
         self.cmd_count = 0
         self.last_log_time = current_time
+
 
 def main(args=None):
     rclpy.init(args=args)

fa847fd9ab92602d93940717f90055dc80cec80c
shay <shay.manor@gmail.com>
2025-12-04 13:45:18 -0500

    feat(pathfinder): update command publishing and added parameter update functionality

--- src/autonomous_kart/autonomous_kart/nodes/manual/pathfinder_node.py
+++ src/autonomous_kart/autonomous_kart/nodes/manual/pathfinder_node.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray, Float32
@@ -12,45 +14,100 @@ class PathfinderNode(Node):
         self.steering = 0.0
         self.speed = 0.0
 
+        self.expected_steering = 0.0
+        self.expected_speed = 0.0
+
         self.cmd_count = 0
         self.last_log_time = self.get_clock().now()
 
         self.declare_parameter("system_frequency", 60)
         self.system_frequency = self.get_parameter("system_frequency").value
 
-        self.declare_parameter('simulation_mode', False)
-        self.sim_mode = self.get_parameter('simulation_mode').value
+        self.declare_parameter("simulation_mode", False)
+        self.sim_mode = self.get_parameter("simulation_mode").value
+
+        self.declare_parameter("max_speed", 50)
+        self.max_speed = self.get_parameter("max_speed").value
+
+        self.declare_parameter("acceleration", 2)
+        self.acceleration = self.get_parameter("acceleration").value
+
+        self.declare_parameter("max_steering", 75)
+        self.max_steering = self.get_parameter("max_steering").value
+
+        self.declare_parameter("steering_accel", 5)
+        self.steering_accel = self.get_parameter("steering_accel").value
 
         # Timer to log average every 5 seconds
         self.create_timer(5.0, self.log_command_rate)
-
-        # Subscriber to opencv pathfinder for angles
-        self.opencv_pathfinder_subscriber = self.create_subscription(
-            Float32MultiArray, "track_angles", self.calculate_path_callback, 5
-        )
+        # Run publish each iteration of hot loop
+        self.create_timer(1.0 / self.system_frequency, self.publish_commands)
 
         # Publisher to motor
         self.motor_publisher = self.create_publisher(Float32, "cmd_vel", 5)
 
-        # # Publisher to steering
+        # Publisher to steering
         self.steering_publisher = self.create_publisher(Float32, "cmd_turn", 5)
+
+        self.metrics_publisher = self.create_publisher(
+            Float32MultiArray, "pathfinder_params", 5
+        )
 
         self.logger.info("Initialize Pathfinder Node")
 
-    def calculate_path_callback(self, msg: Float32MultiArray):
-        """
-        Calculate commands for steering and motor from opencv_pathfinder efficiently
-        Part of hot loop so must be efficient
-        :param msg: Float32MultiArray of [left angle from center to base of track from image, right angle ...]
-        :return: Publishes commands to motor & steering
-        """
+    def update_params(self, speed: Optional[float], steering: Optional[float]):
+        """ Updates speed & steering params from given values """
         self.cmd_count += 1
-        self.angles = (msg.data[0], msg.data[1])
 
+        if abs(speed) > self.max_speed:
+            speed = self.max_speed if speed > 0 else -1 * self.max_speed
+        if abs(steering) > self.max_steering:
+            speed = self.max_steering if steering > 0 else -1 * self.max_steering
 
+        if speed:
+            self.expected_speed = speed
+        if steering:
+            self.expected_steering = steering
 
-        self.steering_publisher.publish(Float32(data=self.steering_angle))
-        self.motor_publisher.publish(Float32(data=self.motor_speed))
+        d_speed = self.speed - self.expected_speed
+        d_steer = self.steering - self.expected_steering
+
+        # Update speed by difference or acceleration, whichever abs value is smaller
+        if d_speed != 0:
+            self.speed += (
+                d_speed if abs(d_speed) < self.acceleration else self.acceleration
+            )
+        if d_steer != 0:
+            self.steering += (
+                d_steer
+                if abs(d_steer) < self.steering_accel
+                else (d_steer / abs(d_steer)) * self.steering_accel
+            )
+        self.publish_commands()
+
+    def publish_commands(self):
+        """ Publishes commands from params to steering & motor """
+        self.steering_publisher.publish(Float32(data=self.steering))
+        self.motor_publisher.publish(Float32(data=self.speed))
+
+        time = self.get_clock().now()
+
+        self.metrics_publisher.publish(
+            Float32MultiArray(
+                data=[
+                    self.speed,
+                    self.expected_speed,
+                    self.acceleration,
+                    self.max_speed,
+                    self.steering,
+                    self.expected_steering,
+                    self.steering_accel,
+                    self.max_steering,
+                    time - self.last_log_time,
+                ]
+            )
+        )
+        self.last_log_time = time
 
     def log_command_rate(self):
         """Log average commands per second every 5 seconds"""
@@ -67,6 +124,7 @@ class PathfinderNode(Node):
         # Reset counters
         self.cmd_count = 0
         self.last_log_time = current_time
+
 
 def main(args=None):
     rclpy.init(args=args)

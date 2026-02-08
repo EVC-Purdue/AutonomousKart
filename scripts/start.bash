f78f1b3261a998c914512ef4f5ec5270357aa736
DueroBone <dueromoods@gmail.com>
2026-02-08 20:05:01 +0000

    Had to reorder start.bash to sucsessfully build

--- scripts/start.bash
+++ scripts/start.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 source /opt/ros/humble/setup.bash
+colcon build --symlink-install
 [ -f /ws/install/setup.bash ] && source /ws/install/setup.bash
 [ -f /ws/.venv/bin/activate ] && source /ws/.venv/bin/activate
 
 # iox-roudi -c /etc/iceoryx/roudi_config.toml &
-colcon build --symlink-install
 ros2 launch autonomous_kart bringup_sim.launch.py

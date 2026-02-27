67b695c15ea25ab3c079452b0272cb73807c7258
Danny <179722984+DanielProano@users.noreply.github.com>
2026-02-27 01:13:05 -0500

    Added edge line detection

--- src/autonomous_kart/autonomous_kart/nodes/opencv_pathfinder/label.py
+++ src/autonomous_kart/autonomous_kart/nodes/opencv_pathfinder/label.py
@@ -6,7 +6,7 @@ import time
 # @param y_cutoff: Only consider y percent of image
 # @param colored: Return a colored image
 # @ret: Mask of the road
-def get_img_mask(img: np.ndarray, percent: float=0.7, colored: bool=False):
+def get_img_mask(img: np.ndarray, percent: float=0.6, colored: bool=False):
     if img is None:
         print ('Error opening image!')
         return None
@@ -22,7 +22,7 @@ def get_img_mask(img: np.ndarray, percent: float=0.7, colored: bool=False):
     # Get hue mask
     image_hsv = get_hsv(roi_image)
     lower_red = np.array([0, 0, 100])
-    higher_red = np.array([60, 40, 255])
+    higher_red = np.array([50, 50, 255])
     mask_red = cv.inRange(image_hsv, lower_red, higher_red)
 
     if not colored:
@@ -32,8 +32,13 @@ def get_img_mask(img: np.ndarray, percent: float=0.7, colored: bool=False):
         result = np.zeros_like(image_hsv)
         result[mask_red > 0] = image_hsv[mask_red > 0]
 
-    kernel = np.ones((9, 9), np.uint8)
+    kernel = np.ones((13, 13), np.uint8)
     result = cv.morphologyEx(result, cv.MORPH_OPEN, kernel)
+
+    r = find_road_right(result)
+    l = find_road_left(result)
+
+    draw_lines(result, r, l)
     
     return result
 
@@ -51,10 +56,12 @@ def get_video_mask(vid):
     height, width = f.shape[:2]
 
     fps = vid.get(cv.CAP_PROP_FPS)
+    if fps == 0:
+        fps = 30
 
     video = cv.VideoWriter("labeled_video.mp4", cv.VideoWriter_fourcc(*'mp4v'), fps, (width, height)) 
     vid.set(cv.CAP_PROP_POS_FRAMES, 0)
-    while (vid.isOpened()):
+    while (True):
         ret, frame = vid.read()
 
         if not ret:
@@ -65,8 +72,48 @@ def get_video_mask(vid):
     vid.release()
     video.release()
     cv.destroyAllWindows()
+
+def find_road_right(img):
+    h, w = img.shape[:2]
+    for i in range(h):
+        if img[i, w-3].any():
+            return (w - 3, i)
     
-    return video
+    for i in range(w - 1, w // 2, -1):
+        if img[h - 5, i].any():
+            return (i, h - 5)
+    
+    return (0,0)
+
+def find_road_left(img):
+    h, w = img.shape[:2]
+    for i in range(h):
+        if img[i, 2].any():
+            return (2, i)
+    
+    for i in range(w // 2):
+        if img[h - 5, i].any():
+            return (i, h - 5)
+    
+    return (0,0)
+
+# @param r_coord: Right road coord 
+# @param l_coord: Left road coord
+# @ret: Return img with lines
+def draw_lines(img, r_coord, l_coord):
+    middle = img.shape[1]
+    m: int = middle // 2
+    cv.line(img, (m, 0), r_coord, (200, 0, 200), 3)
+    cv.line(img, (m, 0), l_coord, (200, 0, 200), 3)
+    return img
+
+def get_threshold(img):
+    return cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
+
+def get_contours(thresh):
+    contours, hierachy = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    
+    return contours
 
 # @param: Takes a str file path
 # @ret: Return a video
@@ -98,20 +145,7 @@ def display_img(img):
             return
 
 
-original_img = get_image("/ws/data/internet_test_footage/driver-pov-img.png")
 
-display_img(original_img)
+original_video = get_video("/ws/data/EVC_test_footage/video.mp4")
 
-start = time.perf_counter()
-road_image = get_img_mask(original_img, percent=0.0)
-end = time.perf_counter()
-
-time = start - end
-print(f"Time: {time} secs", time)
-
-display_img(road_image)
-
-
-# original_video = get_video("/ws/data/EVC_test_footage/video.mp4")
-
-# video = get_video_mask(original_video)
+get_video_mask(original_video)

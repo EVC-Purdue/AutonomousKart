50476aa78d5b5a2598848f0777702747357a985d
shb-png <imshriyabalu@gmail.com>
2026-05-14 18:51:17 +0000

    move rosbags to exclusively rubik bringup

--- scripts/start.bash
+++ scripts/start.bash
@@ -10,21 +10,3 @@ pkill -f master_api || true
 # Lat/long are approx west lafayette, not important to be highly accurate
 mkdir -p /ws/logs
 str2str -in ntrip://shayman1:shayman1@108.59.49.226:9000/MSM4_VRS -out tcpsvr://:9195 -b 1 -p 40.4376975222 -86.9444409756 200 >> /ws/logs/str2str.log 2>&1 &
-
-# Logs supervisor - crash tolerant
-(
-  while true; do
-    RUN_NAME="run_$(date -u +%Y%m%d_%H%M%S)"
-    ros2 bag record -a \
-      --storage mcap \
-      --storage-preset-profile zstd_fast \
-      --max-bag-duration 60 \
-      --exclude '/camera/.*' \
-      --output "/ws/logs/$RUN_NAME" \
-      >> /ws/logs/recorder.log 2>&1
-    echo "[$(date -u +%FT%TZ)] recorder exited, restarting in 1s" >> /ws/logs/recorder.log
-    sleep 1
-  done
-) &
-
-sleep infinity
--- src/autonomous_kart/autonomous_kart/launch/bringup_rubik.launch.py
+++ src/autonomous_kart/autonomous_kart/launch/bringup_rubik.launch.py
@@ -1,17 +1,31 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, GroupAction, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, SetParameter, SetParametersFromFile
 from ament_index_python.packages import get_package_share_directory
+from datetime import datetime
 import os
 
 
 def generate_launch_description():
     pkg_share = get_package_share_directory("autonomous_kart")
-
     system_yaml = os.path.join(pkg_share, "params", "system.yaml")
-
+    run_name = datetime.now().strftime("run_%Y%m%d_%H%M%S")
+    bag_output_path = os.path.join("/ws/logs", run_name)
     sim_mode = LaunchConfiguration("simulation_mode")
+    rosbag_recorder = ExecuteProcess(
+            cmd=[
+                'ros2', 'bag', 'record', '-a',
+                '--storage', 'mcap',
+                '--storage-preset-profile', 'zstd_fast',
+                '--max-bag-duration', '60',
+                '--exclude', '/camera/.*',
+                '--output', bag_output_path
+            ],
+            output='screen',
+            respawn=True,
+            respawn_delay=1.0
+        )
 
     return LaunchDescription(
         [
@@ -20,6 +34,8 @@ def generate_launch_description():
                 [
                     SetParametersFromFile(system_yaml),
                     SetParameter(name="simulation_mode", value=sim_mode),
+
+                    rosbag_recorder,
 
                     Node(
                         package="autonomous_kart",

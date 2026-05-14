604fa22bf341411b839743feb03a0e11f3c05e40
shb-png <imshriyabalu@gmail.com>
2026-05-14 15:54:01 -0400

    idk

--- scripts/start.bash
+++ scripts/start.bash
@@ -10,3 +10,5 @@ pkill -f master_api || true
 # Lat/long are approx west lafayette, not important to be highly accurate
 mkdir -p /ws/logs
 str2str -in ntrip://shayman1:shayman1@108.59.49.226:9000/MSM4_VRS -out tcpsvr://:9195 -b 1 -p 40.4376975222 -86.9444409756 200 >> /ws/logs/str2str.log 2>&1 &
+
+sleep infinity

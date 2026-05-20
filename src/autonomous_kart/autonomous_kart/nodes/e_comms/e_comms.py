603c3e35d39be63202b57273c9ab99a29aae82e4
LelsersLasers <millankumar@gmail.com>
2026-05-19 22:18:16 -0400

    Update running mode to be Urgent Stop and not Idle

--- src/autonomous_kart/autonomous_kart/nodes/e_comms/e_comms.py
+++ src/autonomous_kart/autonomous_kart/nodes/e_comms/e_comms.py
@@ -26,7 +26,7 @@ LOGIC_STATE_VALUE_TO_STRING = {
 RUNNING_MODE_VALUE_TO_STRING = {
     0: "RC",
     1: "AUTONOMOUS",
-    2: "IDLE",
+    2: "URGENT STOP",
 }
 
 @dataclass
@@ -103,7 +103,7 @@ def unpack_adcb_status_message(data: bytes, logger: RcutilsLogger) -> AdcbStatus
     - ID = `0x101` - **Status update**
         - Byte 0: state machine mode + rc mode
             - Bits0-3 = state machine mode (see logic.h::logic_mode_t)
-            - Bit4-5 = Mode (0 = RC, 1 = autonomous, 2 = idle)
+            - Bit4-5 = Mode (0 = RC, 1 = autonomous, 2 = urgent stop)
             - Bits6-7 = reserved
         - Byte 1-2: throttle PWM (uint16_t, little endian), the actual PWM value being sent to the ESC for throttle (1000-2000)
         - Byte 3-4: steering PWM (uint16_t, little endian), the actual PWM value being sent to the servo for steering (1000-2000)
